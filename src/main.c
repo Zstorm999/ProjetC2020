117a7ea120b0eaefbb52b56cb4d4988f70bc05c5
Zstorm999 <46458357+Zstorm999@users.noreply.github.com>
2020-10-15 21:20:15 +0200

    created way of placing objects in the terminal

--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
 #include "include/io.h"
 
 
-int main(){
+int main()
+{
+    system("clear");
 
-    char** arr = loadSpriteFromFile("data/trainUpper.txt");
-    
-    printf("%c\n", arr[2][1]);
-    
+    unsigned int** arr = loadSpriteFromFile("data/trainUpper.txt");
+    unsigned int testChar= L'ↀ';                                              //according to this test the "L" is mandatory
+
+    setlocale(LC_ALL, "");
+
+    printf("test: %lc, %u, %lc, %lc", L'Ð', arr[0][4], testChar, arr[0][4]);       //TODO fix arr content not showing up properly
+    placec(3, 3, L'ↀ');                                                           //at least this shit works
+    printf("\r\n");
+
     return 0;
 }
 
