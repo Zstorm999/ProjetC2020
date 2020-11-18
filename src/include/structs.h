01c934cec8f44e649261589fcd0b8f67fa716d8c
Efuziom <enderEfuziom@gmail.com>
2020-11-18 18:05:05 +0100

    add of struct definition file

---
+++ src/include/structs.h
@@ -0,0 +1,17 @@
+typedef struct rectangle
+{                   //exemple:          x                               0   xMin       xMax
+    int x;          //         +--------+--------+-------------> X    0 +----+----------+--+    "xMin, xMax, yMin, yMax" are meant to select an area
+    int y;          //         |        '                               |                  |    in the rectangle, relative to x, y
+    int yMin;       //       y + ---    +--------+                 yMin +    +----------+  |
+    int yMax;       //         |        |  rect  |                      |    |          |  |
+    int xMin;       //         |        +--------+                 yMax +    +----------+  |
+    int xMax;       //         V                                        +------------------+
+} rectangle;        //         Y
+
+typedef struct sprite
+{
+    rectangle container;
+    char** img;
+    char** maskMap;
+    sprite* nextSprite;
+} sprite;
