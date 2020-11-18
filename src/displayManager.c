73b689c4031a9cc803bcb537a25c654f56a94835
Efuziom <enderEfuziom@gmail.com>
2020-11-18 19:37:06 +0100

    nothing to worry about

--- src/displayManager.c
+++ src/displayManager.c
@@ -7,11 +7,13 @@ void init()
 }
 
 void showSprite(sprite sprite){
-    for(int i=sprite.container.yMin; i<sprite.container.yMax; i++){
-        for(int j=sprite.container.xMin; j<sprite.container.xMax; j++){
+    for(int i=sprite.container.yMin; i<sprite.container.yMax; i++)
+    {
+        for(int j=sprite.container.xMin; j<sprite.container.xMax; j++)
+        {
             //printf("%c", sprite.img[i][j]);                                           //work
             if(((sprite.container.y+i)>=0) && ((sprite.container.x+j)>=0))
-                placec(sprite.container.y+i ,sprite.container.x+j , sprite.img[i][j]);  //almost work
+                placec(sprite.container.y+i, sprite.container.x+j, sprite.img[i][j]);  //almost work
         }
     }
 }
