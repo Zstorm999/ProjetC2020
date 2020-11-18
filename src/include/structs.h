e7a9366b07e2b4448a83374f8bf77b2301b28ac2
Efuziom <45491524+Efuziom@users.noreply.github.com>
2020-11-18 20:44:10 +0100

    Update structs.h
    the chained list of sprite is now multidimensional

--- src/include/structs.h
+++ src/include/structs.h
@@ -13,5 +13,5 @@ typedef struct sprite
     rectangle container; //the sprite will be located at container.x, y, but displayed only for the selected area (cf rectangle)
     char** img;
     char** maskMap;
-    struct sprite* nextSprite;
+    struct sprite** nextSprite; //while rendering a sprite, we must make sure to render every sprites that are overlaying it
 } sprite;
