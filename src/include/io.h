fcfdef7654cdb1b203a7556a910849dfdee2d2f6
Efuziom <enderEfuziom@gmail.com>
2020-11-24 12:02:53 +0100

    showsprite in spritemnger

--- src/include/io.h
+++ src/include/io.h
@@ -9,13 +9,7 @@
 #define MAX_LINES 60
 #define MAX_COLUMNS 200
 
-<<<<<<< Updated upstream
-char*** loadSpriteFromFile(char* pathToFile);  //load a file into a matrix
-void placec(int x, int y, char* symbol);       //print a char at the selected loccation
-=======
 wchar_t** loadSpriteFromFile(char* pathToFile);  //load a file into a matrix
-void placec(int x, int y, unsigned int symbol);       //print a char at the selected loccation
-void showSprite(int** sprite);
->>>>>>> Stashed changes
+void placec(int x, int y, unsigned int symbol);  //print a char at the selected loccation
 
 #endif //_IO_H
