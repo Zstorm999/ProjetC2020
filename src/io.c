93f7eb659ba86ccb298140f3312e0de716e82a0d
Zstorm999 <46458357+Zstorm999@users.noreply.github.com>
2020-11-24 11:56:02 +0100

    fix loadspritefromfile

--- src/io.c
+++ src/io.c
@@ -1,6 +1,10 @@
 #include "include/io.h"
 
+<<<<<<< Updated upstream
 char*** loadSpriteFromFile(char* pathToFile)
+=======
+wchar_t** loadSpriteFromFile(char* pathToFile)
+>>>>>>> Stashed changes
 {
     FILE* file = fopen(pathToFile, "r");
 
@@ -12,8 +16,12 @@ char*** loadSpriteFromFile(char* pathToFile)
 
     printf("passed l13-io\r\n");
     //allocating sprite first dimension
+<<<<<<< Updated upstream
     char*** sprite = (char***)calloc(MAX_LINES, sizeof(char**));
     printf("passed l16-io\r\n");
+=======
+    wchar_t** sprite = (int**)calloc(MAX_LINES, sizeof(wchar_t*));
+>>>>>>> Stashed changes
     if(!sprite){
         fprintf(stderr, "Error while allocating sprite first dimension\n");
         return NULL;
@@ -22,7 +30,11 @@ char*** loadSpriteFromFile(char* pathToFile)
 
     for(int i=0; i<MAX_LINES; i++){
         //allocating sprite second dimension
+<<<<<<< Updated upstream
         sprite[i] = (char**)calloc(MAX_COLUMNS, 3*sizeof(char));
+=======
+        sprite[i] = (wchar_t*)calloc(MAX_COLUMNS, sizeof(wchar_t));
+>>>>>>> Stashed changes
         if(!sprite[i]){
             fprintf(stderr, "Error while allocating sprite second dimension\n");
             free(sprite);
@@ -39,6 +51,7 @@ char*** loadSpriteFromFile(char* pathToFile)
     //now that the sprite is corectly allocated, we can fill it
     //we use strings of 1 to 3 chars, 1 char for ascii and 3 for unicode
 
+<<<<<<< Updated upstream
     for(int i=0; i<MAX_LINES; i++)
     {
         for(int j=0; j<MAX_COLUMNS; j++)
@@ -46,6 +59,12 @@ char*** loadSpriteFromFile(char* pathToFile)
             printf("passed l45-io\r\n");
             long int c = fgetc(file);
             printf("%d:%d\r\n", i, j);
+=======
+    for(int i=0; i<MAX_LINES; i++){
+        for(int j=0; j<MAX_COLUMNS; j++){
+            wchar_t c = fgetwc(file);
+
+>>>>>>> Stashed changes
             if(c == EOF) goto out;
             else
             {
@@ -74,12 +93,29 @@ char*** loadSpriteFromFile(char* pathToFile)
     return sprite;
 }
 
-void destroySprite(char** sprite){
+void destroySprite(int** sprite){
     for(int i=0; i< MAX_COLUMNS; i++){
         free(sprite[i]);
     }
     free(sprite);
 }
+<<<<<<< Updated upstream
+=======
+
+void showSprite(wchar_t** sprite){
+
+    for(int i=0; i<MAX_LINES; i++){
+        for(int j=0; j<MAX_COLUMNS; j++){
+
+            wprintf(L"%lc", sprite[i][j]);
+            if(sprite[i][j] == '\n') break;
+
+        }
+    }
+
+}
+
+>>>>>>> Stashed changes
 /*print a char at the selected coordinates,
 every coordinates originates from the top left corner*/
 void placec(int x, int y, char* symbol)
--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@ int main()
 
     setlocale(LC_ALL, "");
 
+<<<<<<< Updated upstream
     sprite Sprite;
     Sprite.img = loadSpriteFromFile("data/staticMap.txt");
     Sprite.container.x= Sprite.container.y= Sprite.container.xMin= Sprite.container.yMin= 0;
@@ -14,6 +15,16 @@ int main()
     Sprite.container.yMax= MAX_LINES;
     Sprite.nextSprite= NULL;
     showSprite(Sprite);
+=======
+    int** sprite = loadSpriteFromFile("data/staticMap.txt");
+    
+    //wprintf((wchar_t*)("%c"), );
+    //wprintf((wchar_t*)("%c"), );
+    //wprintf((wchar_t*)("%c"), sprite[0][7]);
+    showSprite(sprite);
+    wprintf(L"%lc\n", sprite[3][7]);
+
+>>>>>>> Stashed changes
 
     return 0;
 }
